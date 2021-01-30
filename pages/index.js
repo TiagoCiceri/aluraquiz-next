d29f6eeba84aea2698f76466727f6172ab279107
TiagoCiceri <tiagociceri@gmail.com>
2021-01-30 13:42:33 -0300

    fist commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+ 
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+export const QuizContainer = styled.div` 
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground BackgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Vamos falar de JavaScript</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>ellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz JS</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+          </Widget.Content>
+
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/TiagoCiceri"/>
+    </QuizBackground>
+  )
 }
